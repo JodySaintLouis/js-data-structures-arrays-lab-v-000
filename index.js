e8cd2f056a92d4e9cf30c8709e200f11bfcb6f74
Jamal Gelin <gelin.jamal@gmail.com>
2021-05-22 20:20:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,4 +13,6 @@ function destructivelyRemoveLastDriver(name){
   drivers.pop(name);
 }
 
-function destructivelyRemoveFirstDriver(name)
+function destructivelyRemoveFirstDriver(name){
+  drivers.shift(name);
+}
